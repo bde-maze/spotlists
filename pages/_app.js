4191328abf42a1a118844c80465599f2e7a9d225
Barthélemy de Mazenod <bde.maze@gmail.com>
2021-12-31 22:29:56 +0100

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const Layout = ({ children }) => {
+  return (
+    <div className="w-full min-w-screen min-h-screen p-8 bg-gray-800 text-white">
+      {children}
+    </div>
+  )
 }
 
-export default MyApp
+const SpotList = ({ Component, pageProps }) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default SpotList
